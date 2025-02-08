2d62ad72bea3a15f30865bf6e5ab2eba2fb4d817
Eduard Vilensky <embox53@gmail.com>
2025-02-08 20:20:53 +0200

    working on slug styling

--- src/components/CustomizeProducts.jsx
+++ src/components/CustomizeProducts.jsx
@@ -1,7 +1,53 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import Link from "next/link";
+import Image from "next/image";
+
+const items = [
+  {
+    id: "1",
+    url: "https://images.pexels.com/photos/30586104/pexels-photo-30586104/free-photo-of-elegant-dining-scene-with-red-wine-and-glass.jpeg?auto=compress&cs=tinysrgb&w=800",
+    href: "",
+  },
+  {
+    id: "2",
+    url: "https://images.pexels.com/photos/918327/pexels-photo-918327.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    href: "",
+  },
+  {
+    id: "3",
+    url: "https://images.pexels.com/photos/1854664/pexels-photo-1854664.jpeg?auto=compress&cs=tinysrgb&w=800",
+    href: "",
+  },
+  {
+    id: "4",
+    url: "https://images.pexels.com/photos/1854664/pexels-photo-1854664.jpeg?auto=compress&cs=tinysrgb&w=800",
+    href: "",
+  },
+  {
+    id: "5",
+    url: "https://images.pexels.com/photos/1854664/pexels-photo-1854664.jpeg?auto=compress&cs=tinysrgb&w=800",
+    href: "",
+  },
+];
+
+const TAB_DATA = [
+  {
+    title: "Shipments and returns",
+    id: "shipments",
+    content: <div className=""></div>,
+  },
+  {
+    title: "Description",
+    id: "description",
+    content: <div className=""></div>,
+  },
+];
 
 const CustomizeProducts = () => {
+  const [tab, setTab] = useState("shipments");
+
   return (
     <div>
       <div className="pb-20">
@@ -36,6 +82,34 @@ const CustomizeProducts = () => {
       <div>
         <h1 className="text-2xl py-2">To create the perfect gift!</h1>
         <p className="text-lg">Complementary products to choose from:</p>
+        <div className="flex justify-between mt-8 gap-4 flex-wrap sm:flex-nowrap">
+          {items.map((item, i) => (
+            <div className="w-1/4 h-32 relative cursor-pointer" key={item.id}>
+              <Link href={item.href}>
+                <Image
+                  src={item.url}
+                  fill
+                  sizes="20vw"
+                  alt=""
+                  className="object-cover rounded-md"
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+        <div className="mt-6">
+          <span className="text-lg font-semibold">Share:</span>
+          <div className="flex mt-2 gap-4">
+            <Image src="/images/twitter.png" alt="" width={20} height={20} />
+            <Image src="/images/telegram.png" alt="" width={20} height={20} />
+            <Image
+              src="/images/facebook-share.png"
+              alt=""
+              width={20}
+              height={20}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
